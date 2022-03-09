eab2c1e860a18d48871ebc0993b38c80164d0333
Lucazio <lucas.fallous24@gmail.com>
2022-03-09 10:17:17 +0100

    verifRoot

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from src.verifRoot import VerifRoot
 def main():
     art.randomArt()
     testConnection.checkConnection()
-    print(VerifRoot.isRoot())
+    VerifRoot.testRoot()
     menu = {"00":"full",
     "1":"basique",
     "2":"catégories",
--- src/verifRoot.py
+++ src/verifRoot.py
@@ -1,7 +1,17 @@
 import os
+from typing_extensions import Self
+from .TextColor import TextColor
 
 
 class VerifRoot:
     def isRoot():
         userId = os.geteuid()
         return userId == 0
+
+    def testRoot():
+        print(f"{TextColor.CBOLD}{TextColor.CBLUE}[Test]{TextColor.CEND}{TextColor.CBLUE2} Test utilisateur {TextColor.CEND}")
+        if Self.isRoot():
+            print(f"{TextColor.CBOLD}{TextColor.CGREEN}[Test]{TextColor.CEND}{TextColor.CGREEN2} Vous êtes Root {TextColor.CEND}")
+        else:
+            print(f"{TextColor.CBOLD}{TextColor.CBLINK}{TextColor.CRED}[WARNING]{TextColor.CEND}{TextColor.CRED} Vous n'exécutez pas le programme en tant que root (utilisez sudo){TextColor.CEND}")
+
