95291d8e1b0ec8718e26d2e716e9c54587f2fb76
Lucazio <lucas.fallous24@gmail.com>
2022-03-09 10:20:49 +0100

    verifRoot

--- src/verifRoot.py
+++ src/verifRoot.py
@@ -4,13 +4,14 @@ from .TextColor import TextColor
 
 
 class VerifRoot:
+
     def isRoot():
         userId = os.geteuid()
         return userId == 0
 
-    def testRoot(self):
+    def testRoot():
         print(f"{TextColor.CBOLD}{TextColor.CBLUE}[Test]{TextColor.CEND}{TextColor.CBLUE2} Test utilisateur {TextColor.CEND}")
-        if self.isRoot():
+        if VerifRoot.isRoot():
             print(f"{TextColor.CBOLD}{TextColor.CGREEN}[Test]{TextColor.CEND}{TextColor.CGREEN2} Vous êtes Root {TextColor.CEND}")
         else:
             print(f"{TextColor.CBOLD}{TextColor.CBLINK}{TextColor.CRED}[WARNING]{TextColor.CEND}{TextColor.CRED} Vous n'exécutez pas le programme en tant que root (utilisez sudo){TextColor.CEND}")
