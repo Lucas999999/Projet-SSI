1c572b37a8d187c40d7bcdc477523740428df644
Lucazio <lucas.fallous24@gmail.com>
2022-04-06 10:07:14 +0200

    :alembic: tests

--- src/menu.py
+++ src/menu.py
@@ -1,5 +1,8 @@
+from socket import NI_NUMERICSERV
+from .menuComponent import MenuComponent
+
 class Menu:
-    def __init__(self,name,components,exitMethod,returnMethod):
+    def __init__(self,name,components):
         self.name = name
         self.components = components
 
@@ -7,11 +10,25 @@ class Menu:
 
     def toString(self):
         strRet = f"{self.name}"
-        lenMax = 0
-        compteur = 0
+        lenMax = len(self.name)
 
         for number, component in enumerate(self.components):
-            compteur = number
             if(lenMax< len(f"{component.name} {number}")):
                 lenMax = len(f"{component.name} {number}")
+        print(self.nameToString(lenMax))
+        for number, component in enumerate(self.components):
+            print(component.toString(lenMax, number))
+        print(f"+{'-'*lenMax}+")
 
+    def nameToString(self,lenMin):
+        strRet = self.name
+        compteur = 0
+        while(len(strRet)<lenMin):
+            if(compteur%2 == 1):
+                strRet+="-"
+            else:
+                strRet = "-"+strRet
+        return f"+{strRet}+"
+
+    def execute(self):
+        print("test")
