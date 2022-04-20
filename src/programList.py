1fc37e3db3022f925a1a638501a27189002a9aa8
Lucazio <lucas.fallous24@gmail.com>
2022-04-20 10:20:14 +0200

    :alembic: test

--- src/programList.py
+++ src/programList.py
@@ -11,21 +11,21 @@ class ProgramList:
         # self.GetProgram(index).Install()
         self.GetProgram(index).Install()
 
-    list = [program("nmap","mapping réseau","??",["apt-get install -y nmap"],"informations","nmap --version"),
-    program("netdiscorver","mapping réseau","??",["apt-get install -y netdiscover"],"informations","netdiscover --version"),
-    program("unix-privesc-check","recherche malconfigurations","??",["apt-get install -y unix-privesc-check"],"analyse vuln","??"),
-    program("nikto","web server scanner","??",["apt-get install -y nikto"],"analyse vuln","??"),
+    list = [program("nmap","mapping réseau","??",["apt-get install -y nmap"],["test"],"informations","nmap --version"),
+    program("netdiscorver","mapping réseau","??",["apt-get install -y netdiscover"],["test"],"informations","netdiscover --version"),
+    program("unix-privesc-check","recherche malconfigurations","??",["apt-get install -y unix-privesc-check"],["test"],"analyse vuln","??"),
+    program("nikto","web server scanner","??",["apt-get install -y nikto"],["test"],"analyse vuln","??"),
     ## burpsuite docker ?
-    program("sqlmap","mappage bdd","??",["apt update","apt install -y snapd","snap install core","snap install sqlmap"],"web-app","??"),
-    program("wfuzz","fuzz payload","??",["pip install wfuzz"],"web-app","??"),
-    program("SQLite database browser","edit database files","??",["snap install sqlitebrowser"],"evaluation database","??"),
-    program("john","attaque mots de passe","??",["snap install john-the-ripper"],"attaque mot de passe","??"),
-    program("medusa","password attack","??",["apt-get install -y medusa"],"attaque mot de passe","??"),
-    program("wordlist","password attack","??",["apt install -y wordlists"],"attaque mot de passe","??"),
-    program("hydra","password attack","??",["apt-get install hydra-gtk"],"attaque mot de passe","??"),
-    program("hashcat","password attack","??",["apt-get install hashcat"],"attaque mot de passe","??"),
-    program("patator","password attack","??",["apt-get -y install patator"],"attaque mot de passe","??"),
-    program("spooftooph","bluetooth spoof","??",["apt install spooftooph"],"wireless attack","??")
+    program("sqlmap","mappage bdd","??",["apt update","apt install -y snapd","snap install core","snap install sqlmap"],["test"],"web-app","??"),
+    program("wfuzz","fuzz payload","??",["pip install wfuzz"],["test"],"web-app","??"),
+    program("SQLite database browser","edit database files","??",["snap install sqlitebrowser"],["test"],"evaluation database","??"),
+    program("john","attaque mots de passe","??",["snap install john-the-ripper"],["test"],"attaque mot de passe","??"),
+    program("medusa","password attack","??",["apt-get install -y medusa"],["test"],"attaque mot de passe","??"),
+    program("wordlist","password attack","??",["apt install -y wordlists"],["test"],"attaque mot de passe","??"),
+    program("hydra","password attack","??",["apt-get install hydra-gtk"],["test"],"attaque mot de passe","??"),
+    program("hashcat","password attack","??",["apt-get install hashcat"],["test"],"attaque mot de passe","??"),
+    program("patator","password attack","??",["apt-get -y install patator"],["test"],"attaque mot de passe","??"),
+    program("spooftooph","bluetooth spoof","??",["apt install spooftooph"],["test"],"wireless attack","??")
     # aircrack-ng
     # kismet
     ## reverse enginerie
