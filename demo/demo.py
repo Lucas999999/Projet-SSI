c2e12ff3a259d20760720d93f2b822a65f5ab836
Lucas999999 <lucas.fallous24@gmail.com>
2022-05-04 00:34:00 +0200

    test frauduleux

---
+++ demo/demo.py
@@ -0,0 +1,137 @@
+import os
+from time import sleep
+print("""+-----MENU-----+
+|Installer    0|
+|Désinstaller 1|
+|Vérifier     2|
+|Quitter      3|
++--------------+
+    """)
+input("Que voulez vous faire? : ")
+os.system('cls')
+print("""
++------INSTALLER------+
+|TOUT                0|
+|informations        1|
+|analyse vuln        2|
+|databases           3|
+|mots de passe       4|
+|sans fil            5|
+|reverse enginerie   6|
+|exploitation        7|
+|sniff & spoof       8|
+|maintien de l'accès 9|
+|criminalistique    10|
+|rapports           11|
+|navigation         12|
++---------------------+
+""")
+input("Que voulez vous faire? : ")
+os.system('cls')
+print("""
++------INSTALLER------+
+| Maintien de l'accès |
+|                     |
+|TOUT                0|
+|dbd                 1|
+|powersploit         2|
+|sbd                 3|
+|weevely             4|
+|laudanum            5|
+|exe2hex             6|
+|dns2tcp             7|
+|iodine              8|
+|pwnat               9|
+|proxytunnel        10|
+|mimikatz           11|
++---------------------+
+""")
+input("Que voulez vous faire? : ")
+os.system('cls')
+sleep(0.5)
+print("[\33[32m+\033[0m] dbd : Installation réussie")
+sleep(0.5)
+print("[\33[32m+\033[0m] powersploit : Installation réussie")
+sleep(0.5)
+print("[\33[32m+\033[0m] sbd : Installation réussie")
+sleep(0.5)
+print("[\33[32m+\033[0m] weevely : Installation réussie")
+sleep(0.5)
+print("[\33[32m+\033[0m] laudanum : Installation réussie")
+sleep(0.5)
+print("[\33[32m+\033[0m] exe2hex : Installation réussie")
+sleep(0.5)
+print("[\33[32m+\033[0m] dns2tcp : Installation réussie")
+sleep(0.5)
+print("[\33[32m+\033[0m] iodine : Installation réussie")
+sleep(0.5)
+print("[\33[32m+\033[0m] pwnat : Installation réussie")
+sleep(0.5)
+print("[\33[32m+\033[0m] proxytunnel : Installation réussie")
+sleep(0.5)
+print("[\33[32m+\033[0m] mimikatz : Installation réussie")
+sleep(0.5)
+os.system('cls')
+print("""+-----MENU-----+
+|Installer    0|
+|Désinstaller 1|
+|Vérifier     2|
+|Quitter      3|
++--------------+
+    """)
+input("Que voulez vous faire? : ")
+os.system('cls')
+print("""
++------VERIFIER-------+
+|TOUT                0|
+|informations        1|
+|analyse vuln        2|
+|databases           3|
+|mots de passe       4|
+|sans fil            5|
+|reverse enginerie   6|
+|exploitation        7|
+|sniff & spoof       8|
+|maintien de l'accès 9|
+|criminalistique    10|
+|rapports           11|
+|navigation         12|
++---------------------+
+""")
+input("Que voulez vous faire? : ")
+os.system('cls')
+print("""
++------VERIFIER-------+
+|     Mots de passe   |
+|                     |
+|TOUT                0|
+|John                1|
+|Medusa              2|
+|Hydra               3|
+|Hashcat             4|
+|Patator             5|
++---------------------+
+""")
+input("Que voulez vous faire? : ")
+os.system('cls')
+sleep(0.25)
+print("[\33[31m-\033[0m] John : Pas installé")
+sleep(0.25)
+print("[\33[32m+\033[0m] Medusa : Installé")
+sleep(0.25)
+print("[\33[31m-\033[0m] Hydra : Pas installé")
+sleep(0.25)
+print("[\33[32m+\033[0m] Hashcat : Installé")
+sleep(0.25)
+print("[\33[32m+\033[0m] Patator : Installé")
+sleep(2)
+os.system('cls')
+print("""+-----MENU-----+
+|Installer    0|
+|Désinstaller 1|
+|Vérifier     2|
+|Quitter      3|
++--------------+
+    """)
+input("Que voulez vous faire? : ")
+os.system('cls')
